8a5c90f959ab884efd806b6c0bdd5423ad8fc176
Rupa Dhillon <rupa211984@gmail.com>
2019-04-09 23:11:54 -0700

    Prune old links

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -1,7 +1,7 @@
 class ShortenedUrl < ApplicationRecord
   validates :short_url, :long_url, :user_id, presence: true
   validates :short_url, uniqueness: true
-  validate :no_spamming, on: :create
+  validate :no_spamming, :nonpremium_max, on: :create
 
   before_validation do
     self.short_url = ShortenedUrl.random_code if self.short_url.nil?
@@ -15,7 +15,8 @@ class ShortenedUrl < ApplicationRecord
   has_many :clicks,
     class_name: 'Visit',
     primary_key: :short_url,
-    foreign_key: :short_url
+    foreign_key: :short_url,
+    dependent: :destroy
 
   has_many :visitors,
     Proc.new { distinct },
@@ -25,7 +26,8 @@ class ShortenedUrl < ApplicationRecord
   has_many :taggings,
     class_name: 'Tagging',
     primary_key: :id,
-    foreign_key: :link_id
+    foreign_key: :link_id,
+    dependent: :destroy
 
   has_many :tag_topics,
     through: :taggings,
@@ -56,5 +58,24 @@ class ShortenedUrl < ApplicationRecord
   def no_spamming
     spamming = ShortenedUrl.where("user_id = ? AND created_at > ?", self.user_id, 1.minutes.ago).count >= 5
     errors.add(:user_id, message: 'Too many posts in one minute') if spamming
+  end
+
+  def nonpremium_max
+    if !self.submitter.premium && self.submitter.submitted_urls.count > 4
+      errors.add(:user_id, message: 'Must be Premium user to submit more than 5 links')
+    end
+  end
+
+  def self.prune(n)
+    # Links created more than n minutes ago and never visited
+    # Links not visited in the last n minutes
+    ShortenedUrl
+      .where
+      .not(id: (ShortenedUrl
+        .left_outer_joins(:clicks)
+        .joins(:submitter)
+        .distinct
+        .where('shortened_urls.created_at > ? OR visits.created_at > ? OR users.premium = true', n.minutes.ago, n.minutes.ago))
+      ).destroy_all
   end
 end
